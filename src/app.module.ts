12dff7966ee2fe185d9ae2ca6f8953840f31561c
GitAntarra <antarra.coding.gmail.com>
2023-08-20 23:32:17 +0700

    generate directory users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvConfig } from './utils/config/env.config';
 import { DatabaseConfig } from './utils/config/database.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [EnvConfig, DatabaseConfig],
+  imports: [EnvConfig, DatabaseConfig, UsersModule],
 })
 export class AppModule {}
